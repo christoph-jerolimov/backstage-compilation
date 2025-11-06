d35a013a17dfa1565fbe402ef83256a57d439b35
Christoph Jerolimov <jerolimov+git@gmail.com>
2025-11-06 23:14:58 +0100

    add and configure readme plugin
    Signed-off-by: Christoph Jerolimov <jerolimov+git@gmail.com>

--- backstage/packages/app/src/App.tsx
+++ backstage/packages/app/src/App.tsx
@@ -8,7 +8,7 @@ import { customHomePageModule } from './modules/homepage/module';
 // import playlistPlugin from './modules/playlist/plugin';
 import jiraPlugin from './modules/jira/plugin';
 import mcpChatPlugin from './modules/mcp-chat/plugin';
-import readmePlugin from './modules/readme/plugin';
+// import readmePlugin from './modules/readme/plugin';
 import wheelOfNamesPlugin from './modules/wheel-of-names/plugin';
 
 export default createApp({
@@ -20,7 +20,7 @@ export default createApp({
     // playlistPlugin, // FIXME(christoph-jerolimov): playlist plugin integration doesn't work so far
     jiraPlugin,
     mcpChatPlugin,
-    readmePlugin,
+    // readmePlugin, // FIXME(christoph-jerolimov): readme plugin integration doesn't work so far
     wheelOfNamesPlugin,
   ],
 });
